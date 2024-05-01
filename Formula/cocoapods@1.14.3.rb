66a2b29bf380168b8e641a7cd416dff395b95990
gitmobileadmin <136590540+gitmobileadmin@users.noreply.github.com>
2024-05-01 20:52:07 +0200

    Update cocoapods@1.14.3.rb

--- Formula/cocoapods@1.14.3.rb
+++ Formula/cocoapods@1.14.3.rb
@@ -1,13 +1,34 @@
-class CocoapodsAT1143 < Formula
-  desc "CocoaPods is a dependency manager for Swift and Objective-C Cocoa projects."
-  homepage "https://cocoapods.org"
+class Cocoapods < Formula
+  desc "Dependency manager for Cocoa projects"
+  homepage "https://cocoapods.org/"
   url "https://github.com/CocoaPods/CocoaPods/archive/refs/tags/1.14.3.tar.gz"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "de05766e5771e0cef7af89f73b0e42a1f1c52a76ce1288592cd9511bcd688a9e"
+  license "MIT"
 
+  bottle do
+    sha256 cellar: :any,                 arm64_sonoma:   "3d97509030c7fe205db528e8a721779f996baee9178e648b5d896cca28de85bf"
+    sha256 cellar: :any,                 arm64_ventura:  "bb420dfa19b74875873bb8ae163c5ebde49c17f82c990ab19cb77103ce70fe7c"
+    sha256 cellar: :any,                 arm64_monterey: "033eac5fc9560e801edd519d11eccd91e54b7848fc035e3c6af8d5076195729a"
+    sha256 cellar: :any,                 sonoma:         "2406f691200c97053887c5659e3f1f52aa413904a7ee64d000f1862fabc956d8"
+    sha256 cellar: :any,                 ventura:        "40aafc727a110f10505ee6b7f4f6d051be72dc467b7c1d4a18f5cd09f608c060"
+    sha256 cellar: :any,                 monterey:       "f87f6f0a7a57fb9a8682f9a685bdf7f576d3223480ec90c4bc605f3007c8ddaf"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:   "13e24e0309f634de3154d601f9b65f5f915ada547c3f37340240f150bf89650d"
+  end
+  depends_on "pkg-config" => :build
   depends_on "ruby"
-
+  uses_from_macos "libffi", since: :catalina
   def install
+    if MacOS.version >= :mojave && MacOS::CLT.installed?
+      ENV["SDKROOT"] = ENV["HOMEBREW_SDKROOT"] = MacOS::CLT.sdk_path(MacOS.version)
+    end
+    ENV["GEM_HOME"] = libexec
     system "gem", "build", "cocoapods.gemspec"
     system "gem", "install", "cocoapods-#{version}.gem"
+    # Other executables don't work currently.
+    bin.install libexec/"bin/pod", libexec/"bin/xcodeproj"
+    bin.env_script_all_files(libexec/"bin", GEM_HOME: ENV["GEM_HOME"])
+  end
+  test do
+    system "#{bin}/pod", "list"
   end
 end
